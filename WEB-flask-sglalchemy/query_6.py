3cc888498134f37397e7e1c4b7dac58211b66da5
Ирина Сергеевна Смирнова <35097671+disath1554@users.noreply.github.com>
2023-03-21 17:41:41 +0300

    Update query_6.py

--- WEB-flask-sglalchemy/query_6.py
+++ WEB-flask-sglalchemy/query_6.py
@@ -6,28 +6,6 @@ from data.jobs import Jobs
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
-def main():
-    global_init(input())
-
-    session = create_session()
-    # dictionary of job_id and len of collaboration
-    collaborations = {x.id: len(x.collaborators.split())
-                      for x in session.query(Jobs).all()}
-
-    max_size = max(collaborations.values())
-    # job_id if len collaboration is max
-    ids = [x for x in collaborations if collaborations[x] == max_size]
-
-    # teamleader id if job id in ids
-    temp = [x.team_leader for x in session.query(Jobs).all() if x.id in ids]
-
-    # result
-    teamleaders = session.query(User).filter(User.id.in_(temp)).all()
-
-    for member in teamleaders:
-        print(member.name, member.surname)
-
-
-if __name__ == '__main__':
-    main()
+#
+надо переписать
+#
